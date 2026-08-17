60dafc0580bdfa1b263632b3f367d87ce5313272
joao-pedro-chapichi <chapichijp@gmail.com>
2026-08-17 17:44:19 -0300

    feat: adicionando telas de formulario

--- src/main/java/br/com/fatecads/fatecads/controller/ProdutoController.java
+++ src/main/java/br/com/fatecads/fatecads/controller/ProdutoController.java
@@ -10,7 +10,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import br.com.fatecads.fatecads.entity.Produto;
 import br.com.fatecads.fatecads.service.ProdutoService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
 
 
 @Controller
@@ -27,5 +33,35 @@ public class ProdutoController {
         model.addAttribute("produtos", produtos);
         return "produto/listarProdutos";
     }
+
+    // Método para abrir o formulário de criação de produtos
+    @GetMapping("/criar")
+    public String criarForm(Model model) {
+        model.addAttribute("produto", new Produto());
+        return "produto/formularioProduto";
+    }
+    
+    // Método para salvar o produto no banco de dados
+    @PostMapping("/salvar")
+    public String salvar(@ModelAttribute Produto produto) {
+        produtoService.save(produto);
+        return "redirect:/produtos/listar";
+    }
+    
+    // Método para abrir o formulário de edição de produtos
+    @GetMapping("/editar/{id}")
+    public String editarForm(@PathVariable Integer id, Model model) {
+        Produto produto = produtoService.findById(id);
+        model.addAttribute("produto", produto);
+        return "produto/formulario";
+    }
+    
+    // Método para excluir um produto pelo ID
+    @GetMapping("/excluir/{id}")
+    public String excluir(@PathVariable Integer id) {
+        produtoService.deleteById(id);
+        return "redirect:/produtos/listar";
+    }
+    
     
 }
