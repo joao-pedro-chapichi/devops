e2331a87718cd9a64cebddd9b35cf2ce3134f480
joao-pedro-chapichi <chapichijp@gmail.com>
2026-09-13 22:26:18 -0300

    modificando estilização das páginas do sistema

--- src/main/java/br/com/fatecads/fatecads/security/SecurityConfig.java
+++ src/main/java/br/com/fatecads/fatecads/security/SecurityConfig.java
@@ -18,6 +18,7 @@ public class SecurityConfig {
                                 .csrf(csrf -> csrf.disable())
                                 .authorizeHttpRequests(auth -> auth
                                                 .requestMatchers(
+    "/",
     "/login",
     "/fatecads",
     "/css/**",
